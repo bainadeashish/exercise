bf67110faabe975dfbab7d4d25dde8e273ce75e6
Ashish Bainade <bainadeashish@gmail.com>
2016-07-14 18:53:37 +0530

    First Excersice Commit

---
+++ first.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "This is default page"
+
+@app.route('/profile')
+def users():
+    return render_template("profile.html",name="Ashish")
+
+if __name__== "__main__":
+    app.run(debug=True)
+    
