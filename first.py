29b4157d805852ac504d90e33bcd187516b6c660
Ashish Bainade <bainadeashish@gmail.com>
2016-07-18 14:11:21 +0530

    final

--- first.py
+++ first.py
@@ -1,13 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from test.test_xml_etree import methods
+import os,xlrd
+import pdb
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "This is default page::"
+UPLOAD_FOLDER = 'C:\Users\Ashish.Bainade@infovisionlabs.com\Desktop\Ashish\project\exercise_1\static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/profile')
-def users():
-    return render_template("profile.html",name="Ashish")
+
+def validate_file(file):
+    workbook = xlrd.open_workbook(file)
+    worksheet = workbook.sheet_by_index(0)
+    print "value at row 8,column 2 is :{}".format(worksheet.cell(8,4).value)
+    
+@app.route('/',methods=['POST','GET'])
+def index():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    if request.method == 'POST':
+        file = request.files['file_uploaded']
+        filename=file.filename
+        if filename :
+            file_save_path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_save_path)
+            return render_template('validation.html',file=file.filename)
 
 if __name__== "__main__":
     app.run(debug=True)
